82aca32793c1bcff2628441a7c28b85103da7bec
yandere chan <51772725+yc8690347@users.noreply.github.com>
2019-09-14 07:51:52 -0400

    fciukc asacii

--- index.js
+++ index.js
@@ -4,20 +4,6 @@ exports.nynm = function() {
     }, 1);
 }
 
-exports.nynmsi = function() {
-    let fuck1 = ".-. .-..-.  .-..-.   .-..-. .-.   .-. .----.   .-. .-..----. .----. .----.  .--.  .----.  .----.  .----.";
-    let fuck2 = "|  `| | \ \/ / |  `.'  ||  `| |   | |{ {__     | |/ //  {}  \| {}  }| {_   / {} \ | {}  }/  {}  \/  {}  \";
-    let fuck3 = "| |\  |  }  {  | |\ /| || |\  |   | |.-._} }   | |\ \\      /| .-. \| {__ /  /\  \| {}  }\      /\      /";
-    let fuck4 = "`-' `-'  `--'  `-' ` `-'`-' `-'   `-'`----'    `-' `-'`----' `-' `-'`----'`-'  `-'`----'  `----'  `----'";
-    
-    setInterval(() => {
-        console.log(fuck1);
-        console.log(fuck2);
-        console.log(fuck3);
-        console.log(fuck4);
-    }, 1);
-}
-
 exports.nynmdc = function(bro) { //idk if this works
     setInterval(() => {
         bro.channel.send('NYMN IS KOREABOO');
