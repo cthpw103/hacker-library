2290dc6ce23e490f3ab7793802754c8d7623ff30
cth103 <cthdev@email.com>
2019-09-17 20:48:40 +0100

    ugly
    setinterval to 1 ms wont do shit ok

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 exports.nynm = function() {
     setInterval(() => {
         console.log('NYMN IS KOREABOO');
-    }, 1);
+    }, 1000);
 }
 
 exports.nynmdc = function(bro) { //idk if this works
     setInterval(() => {
         bro.channel.send('NYMN IS KOREABOO');
-    }, 1);
+    }, 1000);
+}
+
+exports.nynmeris = function(msg) {
+    setInterval(() => {
+        msg.channel.createMessage('NYMN IS KOREABOO');
+    }, 1000);
+}
+
+exports.auttaja = function(msg) {
+    setInterval(() => {
+        msg.channel.createMessage('Pong! took 0 ms!');
+    }, 10000);
 }
 
 exports.hack = function() {
     setInterval(() => {
         console.log('HACKING......');
-    }, 1);
+    }, 1000);
 }
